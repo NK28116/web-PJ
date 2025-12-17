74f6e21623d5728819ed1fff658993749afcab9c
NK28116 <shoutarou.phillip.w@gmail.com>
2025-12-17 23:21:55 +0900

    delete Status

--- components/organisms/index.ts
+++ components/organisms/index.ts
@@ -1,5 +1,4 @@
-export { StatusBar } from './StatusBar';
-export type { StatusBarProps } from './StatusBar';
+
 export { Header } from './Header';
 export type { HeaderProps } from './Header';
 export { Modal } from './Modal';
